9c23ce47935252aa1f4c8a7d3b9a8dc2e4700ece
Davide-Mancini <mancinidavide73@gmail.com>
2025-06-12 16:47:16 +0200

    adss

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,30 @@
 const spazioCaselle = document.getElementById("caselle");
-
+// const arrayCaselle = [];
+// arrayCaselle.push(caselle);
 const caselle = function () {
   for (let i = 0; i < 90; i++) {
     const caselleNumeri = document.createElement("div");
-
     spazioCaselle.appendChild(caselleNumeri);
     caselleNumeri.innerText = i + 1;
     caselleNumeri.classList.add("caselleNumeri");
+    // console.log(caselleNumeri[i]);
+    // console.log(caselle.lenght);
+    const bottone = document.getElementById("bottone");
+    bottone.addEventListener("click", function (e) {
+      e.preventDefault;
+      const numeroCasuale = Math.ceil(Math.random() * 90);
+      const numeroEstratto = numeroCasuale;
+
+      console.log(numeroEstratto);
+      const postoPernumero = document.getElementById("postonumero");
+      postoPernumero.innerText = numeroEstratto;
+      const div = document.getElementsByTagName("div");
+      if (numeroEstratto === div[i]) {
+        caselleNumeri.style.backgroundColorcolor = "red";
+      } else {
+      }
+    });
   }
 };
-caselle();
 
-const numeroCasuale = function () {
-  Math.floor(Math.random() * 101);
-};
+caselle();
